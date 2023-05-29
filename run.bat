dcf994f6fbe9818e474b33c181b733c6e6b788ac
DevShayan <shayanahmed393@gmail.com>
2023-05-29 16:50:49 +0500

    removed unnecessary files

--- run.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-/MinGW/bin/g++ main.cpp ui_console/console_ui.cpp -o main.exe
-main.exe
--- run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# g++ main.cpp ./ui_console/console_ui.cpp ./templates/main_page.cpp -o main.out
-g++ main.cpp ./ui_console/tui.cpp ./ui_console/array_options.cpp -o main.out
-./main.out
